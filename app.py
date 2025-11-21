58447a3bc396ce82ce7fe6ab17fb9455c5e8f63f
xandzzz <xandzzz@example.com>
2025-11-21 03:55:11 +0300

    Clean project

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ from fastapi import FastAPI, Request
 from fastapi.responses import HTMLResponse
 from fastapi.templating import Jinja2Templates
 from telegram import Update, KeyboardButton, ReplyKeyboardMarkup, WebAppInfo
-from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
+from telegram.ext import Application, CommandHandler
 
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
@@ -15,34 +15,24 @@ WEBAPP_URL = "https://telegram-mini-adshare-afxx.onrender.com/webapp"
 app = FastAPI()
 templates = Jinja2Templates(directory="templates")
 
-telegram_app = ApplicationBuilder().token(TOKEN).build()
+telegram_app = Application.builder().token(TOKEN).build()
 
-# ---------- /start ----------
-async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
+
+# ---------------- /start ----------------
+async def start(update, context):
     keyboard = [
-        [KeyboardButton(
-            "Открыть мини-приложение",
-            web_app=WebAppInfo(url=WEBAPP_URL)
-        )]
+        [KeyboardButton("Открыть мини-приложение",
+                        web_app=WebAppInfo(url=WEBAPP_URL))]
     ]
 
     markup = ReplyKeyboardMarkup(keyboard, resize_keyboard=True)
     await update.message.reply_text("Добро пожаловать!", reply_markup=markup)
 
+
 telegram_app.add_handler(CommandHandler("start", start))
 
-@app.on_event("startup")
-async def on_startup():
-    await telegram_app.initialize()   # <<< ЭТО ВАЖНО!!
 
-    webhook_url = "https://telegram-mini-adshare-afxx.onrender.com/webhook"
-
-    await telegram_app.bot.delete_webhook(drop_pending_updates=True)
-    await telegram_app.bot.set_webhook(url=webhook_url)
-
-    logger.info("Webhook установлен!")
-
-# ---------- WEBHOOK ----------
+# ---------------- Webhook ----------------
 @app.post("/webhook")
 async def webhook(request: Request):
     data = await request.json()
@@ -50,10 +40,19 @@ async def webhook(request: Request):
     await telegram_app.process_update(update)
     return {"ok": True}
 
-# ---------- ВОТ ОНА /webapp ----------
+
+# ---------------- Webapp page ----------------
 @app.get("/webapp", response_class=HTMLResponse)
 async def webapp(request: Request):
     return templates.TemplateResponse("index.html", {"request": request})
 
-# ---------- STARTUP ----------
 
+# ---------------- Startup ----------------
+@app.on_event("startup")
+async def on_startup():
+    webhook_url = "https://telegram-mini-adshare-afxx.onrender.com/webhook"
+
+    await telegram_app.bot.delete_webhook(drop_pending_updates=True)
+    await telegram_app.bot.set_webhook(url=webhook_url)
+
+    logger.info("Webhook установлен!")
